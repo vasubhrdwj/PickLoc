07febc718284e395c6ed852668204e46e3422dcd
Cole <garcia.ccole@gmail.com>
2023-11-06 16:27:50 +0800

    add carousel

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,7 +1,7 @@
 /** @format */
-import a1 from "../assets/a1.jpg";
-import a2 from "../assets/a2.jpg";
-import a3 from "../assets/a3.jpg";
+import a1 from '../assets/a1.jpg';
+import a2 from '../assets/a2.jpg';
+import a3 from '../assets/a3.jpg';
 
 const Explore = () => {
   return (
@@ -15,21 +15,33 @@ const Explore = () => {
           lifetime. Our trips are designed to leave a lasting impression.
         </p>
       </div>
-      <div className="relative h-[430px]">
-        <img src={a3} alt="photo 1" className="w-full h-full object-cover" />
-        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white">
+      <div className="relative h-[430px] overflow-hidden group">
+        <img
+          src={a3}
+          alt="photo 1"
+          class="w-full h-full object-cover group-hover:scale-110 ease-in-out duration-500 cursor-pointer"
+        />
+        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white ease-in-out group-hover:-translate-y-4 duration-500">
           Road Trips
         </p>
       </div>
-      <div className="relative h-[430px]">
-        <img src={a2} alt="photo 2" className="w-full h-full object-cover" />
-        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white">
+      <div className="relative h-[430px] overflow-hidden group">
+        <img
+          src={a2}
+          alt="photo 2"
+          className="w-full h-full object-cover hover:scale-110 ease-in-out duration-500 cursor-pointer"
+        />
+        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white ease-in-out group-hover:-translate-y-4 duration-500">
           Skiing
         </p>
       </div>
-      <div className="relative h-[430px]">
-        <img src={a1} alt="photo 3" className="w-full h-full object-cover" />
-        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white">
+      <div className="relative h-[430px] overflow-hidden group">
+        <img
+          src={a1}
+          alt="photo 3"
+          className="w-full h-full object-cover hover:scale-110 ease-in-out duration-500 cursor-pointer"
+        />
+        <p className="absolute left-1/2 -translate-x-1/2 bottom-4 text-white border-b border-white ease-in-out group-hover:-translate-y-4 duration-500">
           Tropical Gateways
         </p>
       </div>
--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -1,8 +1,34 @@
 /** @format */
 
-import b1 from "../assets/b1.jpg";
+import b1 from '../assets/b1.jpg';
+import b2 from '../assets/b3.jpeg';
+import b3 from '../assets/b4.jpg';
+import b5 from '../assets/b5.jpeg';
+
+import { useEffect, useState } from 'react';
 
 const LearnMore = () => {
+  const images = [b1, b2, b3, b5];
+  const interval = 5000;
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Increment the current image index
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, interval);
+
+    return () => {
+      // Clear the interval when the component unmounts
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const goToImage = (index) => {
+    setCurrentImage(index);
+  };
+
   return (
     <div className="w-11/12 m-auto md:w-10/12">
       <div className="flex flex-col py-20 md:flex-row gap-10">
@@ -14,7 +40,28 @@ const LearnMore = () => {
           trips are designed to leave a lasting impression.
         </p>
       </div>
-      <img src={b1} alt="photo" />
+      <div className="w-full h-[650px] relative">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+            className="w-full h-full object-cover absolute"
+            style={{ display: index === currentImage ? 'block' : 'none' }}
+          />
+        ))}
+        <div className="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 py-4">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => goToImage(index)}
+              className={`w-6 h-6 rounded-full bg-white focus:outline-none ${
+                index === currentImage ? 'opacity-100' : 'opacity-50'
+              }`}
+            ></button>
+          ))}
+        </div>
+      </div>
       <div className="w-full grid grid-cols-1 gap-5 py-10 md:grid-cols-3">
         <div>
           <h1 className="text-2xl dark:text-white">Excellence</h1>
